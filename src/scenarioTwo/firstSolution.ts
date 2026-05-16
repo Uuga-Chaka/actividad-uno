8b3dc8ca2a6ce7e4067cccc21c137a654f7bb14c
uuga <jpgarias09@gmail.com>
2026-05-16 01:16:38 -0500

    refactor(firstSolution.ts): poor but better organization of code using bridge pattern

--- src/scenarioTwo/firstSolution.ts
+++ src/scenarioTwo/firstSolution.ts
@@ -1,51 +1,73 @@
 import { androidNotifiable, webNotifiable } from "./mockFramework.js";
-import type { NotificationStyle } from "./styles.js";
 
-interface Notifiable {
-  target: string;
-  send: (value: string) => void;
+abstract class Platform {
+  abstract logicToSendNotification(message: string): void;
 }
 
-export class WebNotification implements Notifiable {
-  target: string = "web";
-  style: NotificationStyle;
+abstract class Notification {
+  private platform: Platform;
 
-  constructor({ style }: { style: NotificationStyle }) {
-    this.style = style;
+  constructor(platform: Platform) {
+    this.platform = platform;
   }
 
-  send(value: string) {
-    webNotifiable({
-      message: value,
-      target: this.target,
-      style: this.style.style,
-    });
+  abstract styling(): string;
+
+  send(message: string) {
+    if (this.platform === null) return;
+    this.platform.logicToSendNotification(this.styling() + message);
   }
 }
 
-export class MobileNotification implements Notifiable {
-  target: string = "Mobile";
-  style: NotificationStyle;
-
-  constructor({ style }: { style: NotificationStyle }) {
-    this.style = style;
+class Warning extends Notification {
+  styling(): string {
+    return "[WARNING] - ";
   }
+}
 
-  send(value: string) {
-    androidNotifiable({
-      message: value,
-      target: this.target,
-      style: this.style.style,
-    });
+class Alert extends Notification {
+  styling(): string {
+    return "[ALERT] - ";
+  }
+}
+
+class Message extends Notification {
+  styling(): string {
+    return "[MESSAGE] - ";
+  }
+}
+
+class Confirmation extends Notification {
+  styling(): string {
+    return "[CONFIRMATION] - ";
+  }
+}
+
+class AndroidPlatform extends Platform {
+  logicToSendNotification(message: string): void {
+    androidNotifiable(`[ANDROID]-${message}`);
+  }
+}
+
+class WebPlatform extends Platform {
+  logicToSendNotification(message: string): void {
+    androidNotifiable(`[WEB]-${message}`);
   }
 }
 
 export function exampleOne() {
-//   var alertStyle = new AlertStyle();
-//   var web = new WebNotification({ style: alertStyle });
-//   web.send("This is a web message");
+  // Platform
+  var android = new AndroidPlatform();
+  var web = new WebPlatform();
 
-//   var warningStyle = new WarningStyle();
-//   var mobile = new MobileNotification({ style: warningStyle });
-//   mobile.send("This is a mobile notification");
+  // Notification type
+  var warning = new Warning(android);
+  var alert = new Alert(android);
+  var webConfirmation = new Confirmation(web);
+  var webMessage = new Message(web);
+
+  warning.send("The temperature of your phone is way too hot");
+  alert.send("Hey you heart rate is getting low, are you ok?");
+  webConfirmation.send("Are you sure you want to eat all the food?");
+  webMessage.send("Hello!! How are you?");
 }
