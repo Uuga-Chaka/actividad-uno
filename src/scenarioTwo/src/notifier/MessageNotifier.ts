b9c0c8d4560807daafe115b296f285af1e8234c5
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2026-05-23 10:56:57 -0500

    fix(escenario_dos): removed constructor form places where it wasnt needed and removed .DS_Store files

--- src/scenarioTwo/src/notifier/MessageNotifier.ts
+++ src/scenarioTwo/src/notifier/MessageNotifier.ts
@@ -1,15 +1,6 @@
-import { NotificationRenderer } from "../renderer/NotificationRenderer";
 import { Notifier } from "./Notifier";
 
 export class MessageNotifier extends Notifier {
-  constructor(
-    title: string,
-    message: string,
-    renderer: NotificationRenderer
-  ) {
-    super(title, message, renderer)
-  }
-
   show(): void {
     this.renderer.renderTitle(this.title);
     this.renderer.renderMessage(this.message);
