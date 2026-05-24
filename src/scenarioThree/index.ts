2be629baec5a42035f2112246a863d13c8d91f3e
Moises Plata <moisesplata@Moisess-MacBook-Pro.local>
2026-05-23 20:35:41 -0500

    feat: include script for scenario three

--- src/scenarioThree/index.ts
+++ src/scenarioThree/index.ts
@@ -2,18 +2,25 @@ import { ChatRoom } from "./src/mediator/ChatRoom";
 import { RegularUser } from "./src/user/RegularUser";
 import { AdminUser } from "./src/user/AdminUser";
 
-export function scenarioThree() {
-  const chatRoom = new ChatRoom();
+const chatRoom = new ChatRoom();
 
-  const luis = new RegularUser("Luis", chatRoom);
-  const maria = new RegularUser("Maria", chatRoom);
-  const sebastian = new AdminUser("Sebastian", chatRoom);
+const luis = new RegularUser("Luis", chatRoom);
+const maria = new RegularUser("Maria", chatRoom);
+const sebastian = new AdminUser("Sebastian", chatRoom);
 
-  chatRoom.addUser(luis);
-  chatRoom.addUser(maria);
-  chatRoom.addUser(sebastian);
+console.log('--------- Joining chat --------');
+chatRoom.addUser(luis);
+chatRoom.addUser(maria);
+chatRoom.addUser(sebastian);
 
-  luis.send("Hola a todos!");
-  maria.send("Hey que tal!");
-  sebastian.send("Bienvenidos al grupo");
-}
+console.log('--------- Luis sends a message --------');
+luis.send("Hola a todos!");
+
+console.log('--------- Maria sends a message --------');
+maria.send("Hey que tal!");
+
+console.log('--------- Admin Sebastian sends a message --------');
+sebastian.send("Bienvenidos al grupo");
+
+console.log('--------- Admin kicks a user --------');
+sebastian.kickUser(luis, chatRoom);
